8e3d886eb68d055c0af38971c623e010bd61f19d
Andrés Espejo <hola@andresespejo.com>
2019-10-06 00:16:32 +0200

    Empezando barra navegación

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="inicio")
+     */
+    public function index()
+    {
+        return $this->render('inicio/inicio.html.twig', [
+            'controller_name' => 'DefaultController',
+        ]);
+    }
+}
